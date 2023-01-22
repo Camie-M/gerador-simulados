3775a102626749ea00397e07bddb3d6d021e47b2
Camila Miranda de Moura <camilamouradev@gmail.com>
2023-01-22 11:34:21 -0300

    Initial commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Main = ({ title = 'Boilerplate NEXTJS' }) => (
+  <S.Wrapper>
+    <h1>{title}</h1>
+  </S.Wrapper>
+)
+
+export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  align-items: center;
+  background-color: #e8e8e8;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  width: 100vw;
+`
